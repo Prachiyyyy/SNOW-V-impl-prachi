0b3857ef48a7539316ab542877589f57ae2e639e
Prachiyyyy <144449824+Prachiyyyy@users.noreply.github.com>
2025-08-21 02:23:21 +0530

    Update ghash.h

--- ghash.h
+++ ghash.h
@@ -11,4 +11,5 @@ void ghash_mult(u8 * out, const u8 * x, const u8 * y);
 void ghash_update(const u8 * H, u8 * A, const u8 * data, long long length);
 void ghash_final(const u8 * H, u8 * A, u64 lenAAD, u64 lenC, const u8 * maskingBlock);
 
-#endif // GHASH_H
+
+#endif 
